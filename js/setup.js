efff025c78157bb7ebcf01e46c1353ecdaa7880a
Zhigulskiy <evgeniy1st@users.noreply.github.com>
2020-02-13 15:12:55 +0300

    изменяет механизм валидации имени

--- js/setup.js
+++ js/setup.js
@@ -90,15 +90,15 @@ var closePopup = function () {
 };
 
 var validateNameInput = function () {
+  var message = '';
   if (userNameInput.validity.tooShort) {
-    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    message = 'Имя должно состоять минимум из 2-х символов';
   } else if (userNameInput.validity.tooLong) {
-    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+    message = 'Имя не должно превышать 25-ти символов';
   } else if (userNameInput.validity.valueMissing) {
-    userNameInput.setCustomValidity('Обязательное поле');
-  } else {
-    userNameInput.setCustomValidity('');
+    message = 'Обязательное поле';
   }
+  userNameInput.setCustomValidity(message);
 };
 
 var colorizeWizard = function (arrColor, el, inputEl) {
